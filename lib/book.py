d19ba361eec867740697b5e898ebe2221ed6da4a
Monica Garcia <monicairenegarcia@gmail.com>
2024-03-23 17:37:06 -0700

    complete oop putting it all together lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title="Default", page_count=0):
+        self._title = title
+        self._page_count = page_count
+    
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if (isinstance(page_count, int)):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        pass
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
     
         
